02da3ff653d9dbbab51269128cf32e8e208775a6
Grey-Ark <erickrmgarcia@gmail.com>
2025-06-29 13:28:15 -0600

    Sirve!

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  urlApi: 'http://10.90.1.111:3000/api/v1/',
+  publishKey: 'pk_test_51Rf1VnPEOt89mhaSxnrMRzIECizRtwm6dVMIEymur0ofW6SuzbuyrLjIPQHBvwQYPW6thqMBAosvrigRKAyvPVcN00AfDaBGjX',
+  secretKey: 'sk_test_51Rf1VnPEOt89mhaSjcxOg8E4QWu9ANbZHIdSkoOhkvcgL31VHl88TEKoLpkxVBG53V9Rn9EEOvmDeZs3viSkNt9900Ef2aLnHF'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  urlApi: 'http://localhost:3000/api/v1/',
+  publishKey: 'pk_test_51Rf1VnPEOt89mhaSxnrMRzIECizRtwm6dVMIEymur0ofW6SuzbuyrLjIPQHBvwQYPW6thqMBAosvrigRKAyvPVcN00AfDaBGjX',
+  secretKey: 'sk_test_51Rf1VnPEOt89mhaSjcxOg8E4QWu9ANbZHIdSkoOhkvcgL31VHl88TEKoLpkxVBG53V9Rn9EEOvmDeZs3viSkNt9900Ef2aLnHF'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
